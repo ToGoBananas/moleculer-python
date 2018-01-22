5f37e1f31894f10ef29f5bc6ab47e6b5813d4a87
togobananas <endpoo@gmail.com>
2018-01-22 17:55:28 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'moleculer',
+  packages = ['moleculer'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Moleculer node and client for amqp with json serializer',
+  author = 'Andrei Khaliaukin',
+  author_email = 'endpoo@gmail.com',
+  url = 'https://github.com/ToGoBananas/moleculer-python', # use the URL to the github repo
+  download_url = 'https://github.com/ToGoBananas/moleculer-python/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['moleculer', 'microservices'], # arbitrary keywords
+  classifiers = [],
+)
